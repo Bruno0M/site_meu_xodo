4077723e714e33b2268bb5ac66d62e39255b5863
Bruno <brunomedeirosr09@gmail.com>
2023-06-10 23:56:23 -0300

    finalizado page serviço

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
-addEventListener("scroll", function () {
-  var menu = document.getElementById("menu")
-  if (window.pageYOffset > 0) {
-    menu.classList.remove("hide")
-  } else {
-    menu.classList.add("hide")
-  }
-})
+window.addEventListener("scroll", function() {
+  var menu = document.getElementById("menu");
+  var color = menu.querySelectorAll("#nav a");
 
-// $(document).ready(function(){
-//   $("#banners ul").bxSlider({
-//     mode: 'fade',
-//     captions: true,
-//     slideWidth: 600
-//   });
-// });
+  if (window.pageYOffset > 0) {
+    menu.style.backgroundColor = "white";
+    menu.classList.add("fixed");
+
+    for (var i = 0; i < color.length; i++) {
+      color[i].style.color = "#000";
+    }
+  } else {
+    menu.style.backgroundColor = "transparent";
+    menu.classList.remove("fixed");
+
+    for (var i = 0; i < color.length; i++) {
+      color[i].style.color = "#fff"; 
+    }
+  }
+});
+
+
 
 $(function(){
   $('#feedback-slide .container-slide').bxSlider({
